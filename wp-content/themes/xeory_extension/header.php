c7d43e94998f8e46424ce60494fefe7193fb1ed8
Yuji Sato <yz.xncsk@gmail.com>
2024-03-29 12:44:38 +0900

    タグマネ

--- wp-content/themes/xeory_extension/header.php
+++ wp-content/themes/xeory_extension/header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE HTML>
 <html lang="ja" prefix="og: http://ogp.me/ns#">
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-5KSQN77S');</script>
+	<!-- End Google Tag Manager -->
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
@@ -17,6 +24,10 @@
 	?>
 </head>
 <body <?php body_class();?> itemschope="itemscope" itemtype="http://schema.org/WebPage" ontouchstart="">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KSQN77S"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	<div class="pure-container" data-effect="pure-effect-push">
 		<input type="checkbox" id="pure-toggle-right" class="pure-toggle" data-toggle="right"/>
 		<label class="pure-toggle-label pc-dn" for="pure-toggle-right" data-toggle-label="right"><span class="pure-toggle-icon"></span><span class="pure-toggle-text">MENU</span></label>
