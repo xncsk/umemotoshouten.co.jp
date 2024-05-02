e846007a13b574ec9fcee7152d524a5fd3b5c072
Yuji Sato <yz.xncsk@gmail.com>
2024-05-02 17:50:26 +0900

    Update functions.php

--- wp-content/themes/xeory_extension/functions.php
+++ wp-content/themes/xeory_extension/functions.php
@@ -168,7 +168,20 @@ function my_title_required() {
 }
 add_action( 'admin_head-post-new.php', 'my_title_required' );
 
-
+// サイドメニューを非表示
+function remove_menus() {
+	// remove_menu_page( 'index.php' ); // ダッシュボード
+	// remove_menu_page( 'edit.php' ); // 投稿
+	// remove_menu_page( 'upload.php' ); // メディア
+	// remove_menu_page( 'edit.php?post_type=page' ); // 固定ページ
+	remove_menu_page( 'edit-comments.php' ); // コメント
+	// remove_menu_page( 'themes.php' ); // 外観
+	// remove_menu_page( 'plugins.php' ); // プラグイン
+	// remove_menu_page( 'users.php' ); // ユーザー
+	// remove_menu_page( 'tools.php' ); // ツール
+	// remove_menu_page( 'options-general.php' ); // 設定
+}
+add_action( 'admin_menu', 'remove_menus', 999 );
 //パーマリンクからタクソノミー名を削除
 // function my_custom_post_type_permalinks_set($termlink, $term, $taxonomy){
 // 	return str_replace('/'.$taxonomy.'/', '/', $termlink);
