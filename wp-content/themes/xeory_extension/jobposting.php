3b2050da9b1a4a3aa498057dafeccde6f4a05b4f
Yuji Sato <yz.xncsk@gmail.com>
2024-05-06 14:05:51 +0900

    Update jobposting.php

--- wp-content/themes/xeory_extension/jobposting.php
+++ wp-content/themes/xeory_extension/jobposting.php
@@ -1,25 +1,25 @@
 <script type="application/ld+json"> {
 	"@context" : "http://schema.org/",
 	"@type" : "JobPosting",
-	"title" : "春日部市の内装職人",
-	"description" : "【未経験者大歓迎！】\n事業拡大の為、正社員募集（内装職人）します。\n弊社は春日部市を拠点に、都内や埼玉県内のマンションや店舗などのクロス（壁紙）や床の内装工事をしており、主に内装職人として現場で作業して頂きます。",
-	"datePosted" : "2024-02-29",
-	"validThrough" : "2024-08-28T00:00",
+	"title" : "さいたま市の多能工職人",
+	"description" : "【未経験者大歓迎！】\n事業拡大の為、正社員募集（内装職人）します。\n弊社では、さいたま市を拠点に、首都圏界隈の中古マンションのリフォーム・リノベーション工事の施工管理業務をご担当いただきます。現場を発注者の立場で巡回して、工事の進捗管理や品質のチェックを主にご担当いただきます。時間外も平均で5時間～15時間となっておりますので、ワークライフバランスを取りながら就業できます。",
+	"datePosted" : "2024-05-06",
+	"validThrough" : "2024-11-5T00:00",
 	"employmentType" : "正社員",
 	"hiringOrganization" : {
 		"@type" : "Organization",
-		"name" : "株式会社CHANGE",
-		"sameAs" : "https://change-interior.co.jp",
-		"logo" : "https://change-interior.co.jp/img/logo-squ-posi-min.png"
+		"name" : "<?= COMPANYNAME ?>",
+		"sameAs" : "<?php echo home_url(); ?>",
+		"logo" : "<?php echo home_url(); ?>/img/logo-squ-posi-min.png"
 	},
 	"jobLocation" : {
 		"@type" : "Place",
 		"address" : {
 			"@type" : "PostalAddress",
 			"addressRegion" : "埼玉県",
-			"addressLocality" : "春日部市",
-			"streetAddress" : "備後東8-15-5",
-			"postalCode" : "344-0032",
+			"addressLocality" : "さいたま市",
+			"streetAddress" : "見沼区御蔵19-77",
+			"postalCode" : "337-0033",
 			"addressCountry": "JP"
 		}
 	},
@@ -29,10 +29,9 @@
 		"value": {
 			"@type": "QuantitativeValue",
 			"minValue": 300000,
-			"maxValue": 500000,
 			"unitText": "MONTH"
 		}
 	}
 }
-"workHours": "<p>9:00～18:00</p>",
+"workHours": "<p><?= STARTTIME ?>～<?= ENDTIME ?></p>",
 </script>
