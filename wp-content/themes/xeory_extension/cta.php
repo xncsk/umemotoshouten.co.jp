d2038c09a4ebff62024611221ef88dc394f6de92
Yuji Sato <yz.xncsk@gmail.com>
2024-05-09 09:52:29 +0900

    スマホ

--- wp-content/themes/xeory_extension/cta.php
+++ wp-content/themes/xeory_extension/cta.php
@@ -1,11 +1,14 @@
 <section class="bg-contact relative">
-	<div class="box-contact bg-white-alpha center p4vw">
+	<div class="box-contact bg-white-alpha center p4vw sp-p30">
 		<header class="mb30">
 			<h2 class="smoothText activeTrigger df-jc-center h2 fs5vw">CONTACT</h2>
 		</header>
-		<p class="lh20 mb5vw tac sp-tal">マンションリフォーム・リノベーションに関するお悩みごとが<br>ございましたら、お気軽にお問合せください。</p>
-		<div class="tac df df-ai-baseline df-jc-center">
+		<p class="lh20 mb3vw tac sp-tal">マンションリフォーム・リノベーションに関するお悩みごとが<br class="sp-dn">ございましたら、お気軽にお問合せください。</p>
+		<div class="tac df df-ai-baseline df-jc-center sp-dn">
 			<span class="i-telcircle mr5 fs3vw"></span><span class="fs5vw b"><?= TEL ?></span>
+		</div>
+		<div class="tac pc-dn tab-dn sp-db">
+			<a href="tel:<?= TEL ?>" class="btn is-main btnarrow"><span class="i-telcircle mr5"></span><span>電話する</span></a>
 		</div>
 		<?php
 		/*
